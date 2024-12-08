42dd777b6fa62ba5f5564109de4ca45fe84b8ffe
Arthur Fontaine <0arthur.fontaine@gmail.com>
2024-12-08 23:11:17 +0100

    day8: finish part 1 in Crystal

---
+++ day8/src/day8.cr
@@ -0,0 +1,80 @@
+# TODO: Write documentation for `Day8`
+module Day8
+  _input = read_input()
+  antenna_types = get_antenna_types _input
+
+  # puts "Antenna types: #{antenna_types}"
+  # puts "Indexes of 'A': #{get_all_indexes(_input, 'A')}"
+  # puts "Combinations of 'A': #{get_antenna_combinations(_input, 'A')}"
+  # puts "Antinode indexes of 'A': #{get_antinode_from_pair_of_antenna_indexes(_input, get_antenna_combinations(_input, 'A')[0])}"
+
+  result = [] of {Int32, Int32}
+  antenna_types.each do |antenna_type|
+    antenna_combinations = get_antenna_combinations(_input, antenna_type)
+    antenna_combinations.each do |antenna_combination|
+      get_antinode_positions_from_pair_of_antenna(_input, antenna_combination).each { |antinode_position| result << antinode_position }
+    end
+  end
+
+  puts "Result: #{result.uniq.size}"
+end
+
+def read_input : String
+  File.read("input.txt").strip
+end
+
+def get_antenna_types(input : String) : Array(Char)
+  input.each_char.select { |c| c != '\n' && c != '.' }.to_a.uniq
+end
+
+def get_antenna_combinations(input : String, char : Char)
+  get_all_indexes(input, char).combinations(2).map { |c| {c[0], c[1]} }
+end
+
+def get_all_indexes(input : String, char : Char) : Array(Int32)
+  r = [] of Int32
+  i = 0
+  while i < input.size
+    if input[i] == char
+      r << i
+    end
+    i += 1
+  end
+  r
+end
+
+def get_antinode_positions_from_pair_of_antenna(input : String, antenna_indexes : {Int32, Int32}) : Array({Int32, Int32})
+  row_size = input.index('\n').not_nil!
+  number_of_rows = input.size // row_size
+
+  antenna1_position = get_antenna_position(input, antenna_indexes[0])
+  antenna2_position = get_antenna_position(input, antenna_indexes[1])
+
+  antinode1_position = {antenna1_position[0] - (antenna2_position[0] - antenna1_position[0]), antenna1_position[1] - (antenna2_position[1] - antenna1_position[1])}
+  antinode2_position = {antenna2_position[0] - (antenna1_position[0] - antenna2_position[0]), antenna2_position[1] - (antenna1_position[1] - antenna2_position[1])}
+
+  r = [] of {Int32, Int32}
+  if antinode1_position[0] >= 0 && antinode1_position[0] < number_of_rows && antinode1_position[1] >= 0 && antinode1_position[1] < row_size
+    r << antinode1_position
+  end
+  if antinode2_position[0] >= 0 && antinode2_position[0] < number_of_rows && antinode2_position[1] >= 0 && antinode2_position[1] < row_size
+    r << antinode2_position
+  end
+
+  r
+end
+
+def get_antenna_position(input : String, index : Int32)
+  row_size = input.index('\n').not_nil!
+
+  number_of_breakline = input[0..index].count('\n')
+  real_index = index - number_of_breakline
+
+  {real_index // row_size, real_index % row_size}
+end
+
+def get_antenna_index(input : String, position : {Int32, Int32})
+  row_size = input.index('\n').not_nil!
+
+  position[0] * row_size + position[1]
+end
